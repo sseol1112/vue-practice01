83aa321b7b55309f3846247f787af8e0f30499cd
DESKTOP-S2EVAH2\51 <sglee@the-51.com>
2022-08-22 10:15:32 +0900

    22.08.22 1차커밋

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+        <ul>
+            <li v-for="menu in menus" :key="menu"><a href="#">{{menu}}</a></li>
+        </ul>
+    </div>
+    <div class="box_count">
+        
+        <h1><strong>*</strong> count Number 체크!</h1>
+        <h1>{{ count }}</h1>
+        <button @click="plzus">+ </button>
+        <button @click="minus">-</button>
+
+        <!-- component 사용 -->
+        <ListItem :testProp="arrayVue"></ListItem>
+
+        <!-- 이미지 -->
+        <!-- <div class="img_wrap">
+          <img src="./assets/logo.png" />
+        </div> -->
+
+    </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/ListItem.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      count : 0,
+      name : 'greek',
+      arrayVue : [{
+        'number' : '1',
+        'name' : 'john',
+        'subject' : 'math',
+        'level':'10'
+      },{
+        'number' : '2',
+        'name' : 'tom',
+        'subject' : 'english',
+        'level':'5'
+      },{
+        'number' : '3',
+        'name' : 'amy',
+        'subject' : 'english',
+        'level':'3'
+      }],
+      menus : ['menu1','menu2','menu3','menu4','menu5'],
+    }
+  },
   components: {
-    HelloWorld
-  }
+    ListItem
+  },
+  methods : {
+    plus() {
+      this.count += 1;
+      console.log(this.count);
+
+    },
+    minus() {      
+      let countValue = this.count;
+      if(countValue == 0 ){
+        alert('0 이하로는 내릴 수 없습니다.');
+      } else {
+        this.count -= 1;
+        console.log(this.count);
+      }
+    }
+
+  }  
 }
+
 </script>
 
 <style>
+ul { list-style: none; padding:0; }
+a {text-decoration: 0; color: #2c3e50;}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +85,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 550px;
+  margin:0 auto;
+}
+
+.header ul {display:flex; justify-content: space-around;}
+.header ul li {padding:10px 12px; background: skyblue;}
+.header ul li a{ color:#fff;}
+
+@media screen and (max-width: 383px)  {
+    .header ul li {border:1px solid #000; padding:10px 5px;}
 }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <h1>{{ introText }}</h1>
+    <p v-text="infoText" style="color:#ccc;"></p>
+    <ul class="info-list">
+        <li v-for="itemKey in propItem" :key="itemKey"> 
+            <div class="info-area">
+                <span>{{ itemKey.number }}</span>
+                <h3>{{ itemKey.name }}</h3>
+                <p>{{ itemKey.subject }}</p>
+                <h3>{{ itemKey.level }}</h3>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name : 'powder-compon',
+    data() {
+        return {
+            'introText' : '사용자 정보 리스트 입니다.',
+            'propItem' : this.testProp,
+            'infoText' : 'components와 props로 부모데이터로 받아온 배열 임의의 propItem 변수에 담아 데이터 출력되도록 활용하였음.'
+        }
+    },
+    props : {
+        testProp : Array
+    }
+}
+</script>
+
+<style>
+    .info-list {
+        max-width:350px;
+        margin:0 auto;
+        border-top:1px solid #ccc;
+    }
+    .info-list li{
+        border-bottom:1px solid #ccc;
+    }
+    .info-area {
+        display: flex;
+        justify-content: space-between;
+        align-items:center;
+    }
+    .info-area h3 {
+        flex-grow: 1;
+    }
+</style>
