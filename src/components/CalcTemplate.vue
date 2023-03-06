2c466af252d708045cf645449d672d67767a1ac3
sg51-lee <sglee@the-51.com>
2023-03-06 16:34:23 +0900

    230306 - calculator 컴포넌트 form 형식 변경

--- src/components/CalcTemplate.vue
+++ src/components/CalcTemplate.vue
@@ -13,7 +13,8 @@
         </div>                      
 
         <div class="result_box">
-        <h5>결과값<span class="num"></span></h5>
+            <h5>결과값</h5>
+            <input type="text" class="result" name="result-value" value=""/>
         </div>
     </div>
 </template>
@@ -32,7 +33,7 @@ export default {
             let num1 = parseInt(document.querySelector(".calcul_box .form_box input[name=num1]").value);
             let num2 = parseInt(document.querySelector(".calcul_box .form_box input[name=num2]").value);
             let num3 = '';
-            let result = document.querySelector(".result_box h5 .num");
+            let result = document.querySelector(".result_box .result");
             let typeVal = e.target.innerText;
 
             if((num1 != 0 && num2 != 0) && (!num1 || !num2)) {
@@ -41,19 +42,19 @@ export default {
                 switch(typeVal) {
                     case "+" :          
                         num3 = num1 + num2;
-                        result.innerHTML = parseInt(num3);
+                        result.value = parseInt(num3);
                         break;
                     case "-" :
                         num3 = num1 - num2;
-                        result.innerHTML = parseInt(num3);
+                        result.value = parseInt(num3);
                         break;
                     case "*" :
                         num3 = num1 * num2;
-                        result.innerHTML = parseInt(num3);
+                        result.value = parseInt(num3);
                         break;
                     case "/" :
                         num3 = num1 / num2;
-                        result.innerHTML = parseInt(num3);
+                        result.value = parseInt(num3);
                         break;
                     default :
                         alert("잘못된 계산식입니다. 내용을 확인해주세요.");
@@ -69,5 +70,25 @@ export default {
     border: 1px solid #222;
     margin: 10px 0;
     padding: 10px;
+}
+
+.calcul_box {
+  .form_box {
+    display: flex;  
+    margin-top: 20px;
+    input {
+      width:calc(50% - 10px);
+      margin: 0 5px;
+      text-align: center;
+    }
+  }
+  .result_box {
+    h5 {
+        margin: 10px 0;
+    }
+    input {
+        text-align: center;
+    }
+  }
 }
 </style>
