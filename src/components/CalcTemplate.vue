75941dd6371b036f012d00afc3e109f3c0745aca
sg51-lee <sglee@the-51.com>
2023-03-08 17:08:03 +0900

    230308 vue 소스수정

--- src/components/CalcTemplate.vue
+++ src/components/CalcTemplate.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="calcul_box">
-        <h5>두 수를 입력 후 원하는 연산 버튼을 클릭해주세요.</h5>
+        <div class="head_box">
+            <h5>두 수를 입력 후 원하는 연산 버튼을 클릭해주세요.</h5>
+            <button @click="resetFunc();" class="reset_btn">값 초기화</button>
+        </div>        
 
         <button @click="calcFunc">+</button>
         <button @click="calcFunc">-</button>
@@ -60,19 +63,45 @@ export default {
                         alert("잘못된 계산식입니다. 내용을 확인해주세요.");
                 }
             }
+        },
+        resetFunc() {
+            let num1El = document.querySelector(".calcul_box .form_box input[name=num1]");
+            let num2El = document.querySelector(".calcul_box .form_box input[name=num2]");
+
+            if(num1El.value.length > 0 || num2El.value.length > 0) {
+                num1El.value = "";
+                num2El.value = "";
+                alert("값 초기화 완료");
+            } else {
+                alert("입력한 값이 없습니다.");
+            }
         }
     }
 }
 </script>
 
 <style lang="scss">
-.calcul_box {
-    border: 1px solid #222;
-    margin: 10px 0;
-    padding: 10px;
-}
 
 .calcul_box {
+  border: 1px solid #222;
+  margin: 10px 0;
+  padding: 10px;
+  .head_box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 12px;
+    h5 {
+        font-size: 16px;
+        line-height: 21px;
+        margin: 0 5px;
+    }
+    button {
+        background:#fff;
+        color: #222;
+        border: 1px solid #222;
+    }
+  }
   .form_box {
     display: flex;  
     margin-top: 20px;
